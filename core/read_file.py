bf7a747109463bbf4f8792e2575c7477e577db59
Nana Adjei Manu <n.k.a.manu06@gmail.com>
2025-04-08 21:13:19 +0100

    chore: accurately count lost-key.txt
    currently uses a fuzzy algorithm to convert the text into a single file

--- core/read_file.py
+++ core/read_file.py
@@ -1,10 +1,20 @@
 from collections import Counter
 
 def read_file(filename: str):
-    file_content = None
+    file_content = []
     with open(filename, "r") as f:
-        file_content = f.read().strip().split(" ")
-    return file_content
+        sentences = f.read().strip().split(".")
+        for sentence in sentences:
+            words_in_sentence = sentence.strip("").split(" ")
+            single_sentence = " ".join(words_in_sentence)
+            file_content.append(single_sentence.strip())  
+    content = []
+    for s in file_content:
+        words = s.split()
+        content.append(" ".join(s.split(" ")))
+    print(" ".join(content))
+
+    return " ".join(content).split(" ")
         
 def count_unique_words(file_content):
     return Counter(read_file(file_content))
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from core.read_file import count_unique_words
 
 def main():
-    print(count_unique_words("lost-key.txt"))
+   print(count_unique_words("lost-key.txt"))
 
 
 if __name__ == "__main__":
